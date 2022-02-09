311f1534fb65439a250d021c918f0064ef835333
ivan-mukovnin <mukovnin06@yandex.ru>
2022-02-09 13:07:15 +0300

    lesson1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptive = 6;
+
+alert('hello');
+
+console.log('привет');
+
